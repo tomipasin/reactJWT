14bb7cf710b2b993d80bd6c12a3944a111ed0775
tomipasin <tomipasin@gmail.com>
2021-02-17 17:01:20 -0300

    v1.1

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import authHeader from './auth-header';
 
 //const API_URL = 'http://localhost:8080/api/test/';
-const API_URL = 'https://jwtautentication.herokuapp.com/api/test/'
+const API_URL = 'https://jwtautentication.herokuapp.com:8080/api/test/'
 
 class UserService {
   getPublicContent() {
