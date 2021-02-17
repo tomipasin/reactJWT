113cf0937013cc37e144ab8b970b6d182b059b20
tomipasin <tomipasin@gmail.com>
2021-02-17 16:55:44 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { component } from "react";
+import React, { Component } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
