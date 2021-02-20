b5d3948920faeab5670eca6857e82b0fc370d78b
tomipasin <tomipasin@gmail.com>
2021-02-20 11:15:54 -0300

    v1.3

--- src/components/board-user.component.js
+++ src/components/board-user.component.js
@@ -37,9 +37,17 @@ export default class BoardUser extends Component {
     return (
       <div className="container">
         <header className="jumbotron shadow bccard">
-
+        <h4>English version:</h4>
           <div class="embed-responsive embed-responsive-4by3">
             <iframe class="embed-responsive-item" src="https://tomipasin.com/cv/EuroCV___EN___V3.pdf" allowfullscreen></iframe>
+          </div>
+            <hr></hr>
+            <p></p>
+            
+            <h4>Versão em português:</h4>
+            <p></p>
+          <div class="embed-responsive embed-responsive-4by3">
+            <iframe class="embed-responsive-item" src="https://tomipasin.com/cv/EuroCV_PT_V7.pdf" allowfullscreen></iframe>
           </div>
 
         </header>
